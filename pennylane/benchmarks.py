9abd1bee940e8f59a94e100694f23abcc001b16f
Saveliy Yusufov <sy2685@columbia.edu>
2023-05-17 20:51:35 -0400

    Update pennylane benchmark

--- pennylane/benchmarks.py
+++ pennylane/benchmarks.py
@@ -9,65 +9,44 @@ if __name__ == "__main__":
 import pennylane as qml
 import numpy as np
 
-class QCBM:
-    def __init__(self, n, nlayers, dev="lightning.qubit"):
-        self.n = n
-        self.nlayers = nlayers
-        self.neighbors = [(i, (i + 1) % n) for i in range(n)]
-        self.dev = qml.device(dev, wires=n)
-        self.operations = []
 
-    def __call__(self, vars):
-        def apply_qcbm_circuit(vars):
-            self.first_layer(vars[1])
-            for each in vars[2:-2]:
-                self.entangler()
-                self.mid_layer(each)
+def first_rotation(circuit, nqubits):
+    for k in range(nqubits):
+        circuit.append(qml.RX(np.random.rand(), wires=k))
+        circuit.append(qml.RZ(np.random.rand(), wires=k))
 
-            self.entangler()
-            self.last_layer(vars[-1])
 
-            self.dev.apply(self.operations)
+def mid_rotation(circuit, nqubits):
+    for k in range(nqubits):
+        circuit.append(qml.RZ(np.random.rand(), wires=k))
+        circuit.append(qml.RX(np.random.rand(), wires=k))
+        circuit.append(qml.RZ(np.random.rand(), wires=k))
 
-        return apply_qcbm_circuit(vars)
 
-    def generate_random_vars(self):
-        vars = [np.random.rand(self.n, 2)]
-        vars += [np.random.rand(self.n, 3) for _ in range(self.n - 2)]
-        vars += [np.random.rand(self.n, 2)]
-        return vars
+def last_rotation(circuit, nqubits):
+    for k in range(nqubits):
+        circuit.append(qml.RZ(np.random.rand(), wires=k))
+        circuit.append(qml.RX(np.random.rand(), wires=k))
 
-    def first_rotation(self, alpha, beta, wires=1):
-        self.operations.append(qml.RX(alpha, wires=wires))
-        self.operations.append(qml.RZ(beta, wires=wires))
 
-    def mid_rotation(self, alpha, beta, gamma, wires=1):
-        self.operations.append(qml.RZ(alpha, wires=wires))
-        self.operations.append(qml.RX(beta, wires=wires))
-        self.operations.append(qml.RZ(gamma, wires=wires))
+def entangler(circuit, nqubits, pairs):
+    for a, b in pairs:
+        circuit.append(qml.CNOT(wires=[a, b]))
 
-    def last_rotation(self, alpha, beta, wires=1):
-        self.operations.append(qml.RX(alpha, wires=wires))
-        self.operations.append(qml.RZ(beta, wires=wires))
 
-    def first_layer(self, vars):
-        for each in vars:
-            self.first_rotation(each[0], each[1])
+def build_circuit(nqubits, depth, pairs):
+    circuit = []
+    first_rotation(circuit, nqubits)
+    entangler(circuit, nqubits, pairs)
+    for k in range(depth):
+        mid_rotation(circuit, nqubits)
+        entangler(circuit, nqubits, pairs)
 
-    def mid_layer(self, vars):
-        for each in vars:
-            self.mid_rotation(each[0], each[1], each[2])
-
-    def last_layer(self, vars):
-        for each in vars:
-            self.last_rotation(each[0], each[1])
-
-    def entangler(self):
-        for i, j in self.neighbors:
-            self.operations.append(qml.CNOT(wires=[i, j]))
+    last_rotation(circuit, nqubits)
+    return circuit
 """
 
     runner = pyperf.Runner()
 
-    for i in range(4, 26):
-        runner.timeit(f"qcbm {i}", stmt='qcbm(vars)', setup=setup+f"n = {i}; qcbm = QCBM(n, 9); vars = qcbm.generate_random_vars();")
+    for i in range(4, 27):
+        runner.timeit(f"qcbm {i}", stmt='st.apply(circuit)', setup=setup+f"nqubits = {i}; pairs = [(i, (i + 1) % nqubits) for i in range(nqubits)]; circuit = build_circuit(nqubits, 9, pairs); st = qml.device('lightning.qubit', nqubits);")
